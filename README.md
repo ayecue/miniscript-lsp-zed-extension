ae111334cd2fe34b84c7ce9d9783371d7f06dc3e
ayecue <soerenwehmeier@googlemail.com>
2025-03-05 22:35:03 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # miniscript-lsp-zed-extension
+
+This extension uses the [MiniScript Languageserver](https://github.com/ayecue/miniscript-languageserver). 
+
+## Configuration
+
+Use any of the [configuration settings](https://github.com/ayecue/miniscript-languageserver/tree/main/packages/node#configuration) that miniscript-languageserver supports within your zed config under `miniscript.settings`.
---
+++ src/miniscript.rs
@@ -0,0 +1,100 @@
+use std::{env, fs};
+use zed_extension_api::{self as zed, serde_json, settings::LspSettings, LanguageServerId, Result};
+
+const SERVER_PATH: &str = "node_modules/.bin/miniscript-languageserver";
+const PACKAGE_NAME: &str = "miniscript-languageserver";
+
+struct MiniScriptExtension {
+    did_find_server: bool,
+}
+
+impl MiniScriptExtension {
+    fn server_exists(&self) -> bool {
+        fs::metadata(SERVER_PATH).map_or(false, |stat| stat.is_file())
+    }
+
+    fn server_script_path(&mut self, language_server_id: &LanguageServerId) -> Result<String> {
+        let server_exists = self.server_exists();
+        if self.did_find_server && server_exists {
+            return Ok(SERVER_PATH.to_string());
+        }
+
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let version = zed::npm_package_latest_version(PACKAGE_NAME)?;
+
+        if !server_exists
+            || zed::npm_package_installed_version(PACKAGE_NAME)?.as_ref() != Some(&version)
+        {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+            let result = zed::npm_install_package(PACKAGE_NAME, &version);
+            match result {
+                Ok(()) => {
+                    if !self.server_exists() {
+                        Err(format!(
+                            "installed package '{PACKAGE_NAME}' did not contain expected path '{SERVER_PATH}'",
+                        ))?;
+                    }
+                }
+                Err(error) => {
+                    if !self.server_exists() {
+                        Err(error)?;
+                    }
+                }
+            }
+        }
+
+        self.did_find_server = true;
+        Ok(SERVER_PATH.to_string())
+    }
+}
+
+impl zed::Extension for MiniScriptExtension {
+    fn new() -> Self {
+        Self {
+            did_find_server: false,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        _worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let server_path = self.server_script_path(language_server_id)?;
+        Ok(zed::Command {
+            command: zed::node_binary_path()?,
+            args: vec![
+                env::current_dir()
+                    .unwrap()
+                    .join(&server_path)
+                    .to_string_lossy()
+                    .to_string(),
+                "--stdio".to_string(),
+            ],
+            env: Default::default(),
+        })
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<zed::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+
+        let mut map = serde_json::Map::new();
+        map.insert(language_server_id.to_string(), settings);
+        Ok(Some(serde_json::json!(map)))
+    }
+}
+
+zed::register_extension!(MiniScriptExtension);
